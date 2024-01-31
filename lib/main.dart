ab71fdd8d8a1de636136ebfa06cc72e05203b347
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-01-31 13:32:23 +0700

    stateless & stateful

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: Heading(
-            text:"Hello world!"),
+          child: BiggerText(text: "Assalamualaikum"),
         ),
       ),
     );
@@ -34,6 +33,37 @@ class Heading extends StatelessWidget {
     return Text(
       text,
       style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget{
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+    _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText>{
+  double _textSize = 16.0;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: (){
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
